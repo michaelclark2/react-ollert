f39c307d71833a3c91a4d814341be0453213d724
michael <michael.clark1992@gmail.com>
2018-07-05 08:53:40 -0500

    added input and linked to state

--- src/components/Boards/Boards.js
+++ src/components/Boards/Boards.js
@@ -3,6 +3,18 @@ import './Boards.css';
 import BoardTile from '../BoardTile/BoardTile';
 
 class Boards extends React.Component {
+  state = {
+    isAdding: false,
+    boardName: '',
+  }
+  addBoard = (e) => {
+    this.setState({isAdding: true});
+  }
+  changeBoardName = (e) => {
+    const newName = e.target.value;
+    this.setState({boardName: newName});
+  }
+
   render () {
     const {user, boards} = this.props;
     const boardComponents = boards.map(board => {
@@ -10,11 +22,17 @@ class Boards extends React.Component {
         <BoardTile key={board.id} board={board}/>
       );
     });
-    boardComponents.push((<button className="btn btn-danger"><span className="glyphicon glyphicon-plus"></span></button>));
+    if (this.state.isAdding) {
+      boardComponents.push((<div className="col-md-3 panel panel-primary"><input onChange={this.changeBoardName} value={this.state.boardName} type="text"/><button className="btn btn-danger"><span className="glyphicon glyphicon-plus"></span></button></div>));
+    } else {
+      boardComponents.push((<button onClick={this.addBoard} className="btn btn-danger"><span className="glyphicon glyphicon-plus"></span></button>));
+    }
     return (
-      <div className="Boards">
-        <h1>Welcome to Ollert</h1>
-        {user ? boardComponents : <h2>Log in to view your boards</h2>}
+      <div className="container-fluid">
+        <div className="Boards">
+          <h1>Welcome to Ollert</h1>
+          {user ? boardComponents : <h2>Log in to view your boards</h2>}
+        </div>
       </div>
     );
   }
