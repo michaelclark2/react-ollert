9f3f7bf9099e708b23f0f87e2ea746b7476ffd47
michael <michael.clark1992@gmail.com>
2018-07-04 22:33:33 -0500

    added google auth

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import './App.css';
+import firebase from 'firebase';
 import connection from '../firebase/connection';
 import Navbar from '../components/Navbar/Navbar';
 
 class App extends React.Component {
+  state = {
+    username: '',
+    user: null,
+  }
+  updateUser = (user) => {
+    this.setState({user});
+  }
+  checkLogin () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // User is signed in.
+        this.setState({
+          user,
+          username: user.displayName,
+        });
+      } else {
+        // No user is signed in.
+      }
+    });
+  }
   componentDidMount () {
     connection();
+    this.checkLogin();
   }
   render () {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar user={this.state.user} setUser={this.updateUser}/>
       </div>
     );
   }
